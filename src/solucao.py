44cdcbc031002e2e19d237f39a55cbf7fb5bce07
Isabela <isacaronelacerda@gmail.com>
2025-12-01 17:29:06 -0300

    deu umas coisas erradas, fiquei estressada kkkk apageui tudo e to indo de nov

--- src/solucao.py
+++ src/solucao.py
@@ -14,3 +14,24 @@ def extrair_nr_dias(nome_dia: str) -> int:
 
 
 
+# função pedida create_solution
+def create_solution(excel_path: str) -> dict:
+    '''realiza a leitura do arquivo excel e cria o dicionario com a solucao
+        levando em consideração as regras de:
+        - prioridade (z, a, b e c)
+        - condição (operando ou parada)
+        - capacidade de recursos
+        - predecessoras
+    '''
+
+
+    # >> 1 leitura do arquivo excel
+    os_df = pd.read_excel(excel_path, sheet_name='OS')
+    tarefas_df = pd.read_excel(excel_path, sheet_name='Tarefas')
+    recursos_df = pd.read_excel(excel_path, sheet_name='Recursos')
+    paradas_df = pd.read_excel(excel_path, sheet_name='Paradas')
+
+    # >> 2 calculo de duração por quantidade POR OS 
+
+    tarefas_df["Demanda_horas"] = tarefas_df["Duração"] * tarefas_df["Quantidade"]
+    
