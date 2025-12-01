279917d63b0d5a08ae15af4ce384b932d4c56a80
Isabela <isacaronelacerda@gmail.com>
2025-12-01 14:29:29 -0300

    instalação da biblioteca openpyxl

--- src/solucao.py
+++ src/solucao.py
@@ -11,7 +11,18 @@ def create_solution(excel_path: str) -> dict:
     recursos_df = pd.read_excel(excel_path, sheet_name='Recursos')
     paradas_df = pd.read_excel(excel_path, sheet_name='Paradas')
 
-    print(tarefas_df.head()) 
-    return {
-    }
 
+
+    tarefas_df = tarefas_df.copy()  # por segurança só 
+    tarefas_df['duração da demanda'] = tarefas_df['duração'] * tarefas_df['quantidade']
+
+    # soma hrs por OS e habilidades 
+    hora_por_OS_habilidade = tarefas_df.groupby(['OS', 'habilidade'])['duração da demanda'].sum().reset_index()
+    print(hora_por_OS_habilidade)
+    
+    return {}
+
+
+if __name__ == "__main__":
+    caminho_arquivo = r"C:\Users\isaca\Desktop\desafio\data\backlog_desafio_500.xlsx"
+    create_solution(caminho_arquivo)
